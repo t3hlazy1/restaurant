b3e3be219f6b288e0fbaa8b4d31451f04829be23
t3hlazy1 <lilfootballplaya12@hotmail.com>
2014-08-16 13:38:40 -0500

    Added some content

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 include 'header.html';
-echo "<h1>Welcome!</h1><p>Finding a restaurant that you will <i>want</i> to eat at has never been easier. <a href=''>Sign up</a> today to
-        experience a new way to dine out.";
+echo "<div class='left'><h1>Welcome!</h1><p>Finding a restaurant that you will <i>want</i> to eat at has never been easier. <a href=''>Sign up</a> today to
+        experience a new way to dine out.</div>";
+echo "<div class='right'><h1>Log-In</h1><form action='login.php'><input type='text' value='User Name' length='16' name='username'><br><input type='password' name='password'></form><br>
+        Don't have an account? <a href='register.php'>Register</a></div>";
 include 'footer.html';
 ?>
