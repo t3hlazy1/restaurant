57435b587da264dcfc4c27e04f9fad5d990162e0
t3hlazy1 <lilfootballplaya12@hotmail.com>
2014-08-16 13:21:04 -0500

    Color changes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'header.html';
-echo "5:23";
-echo "test"; 
+echo "<h1>Welcome!</h1><p>Finding a restaurant that you will <i>want</i> to eat at has never been easier. <a href=''>Sign up</a> today to
+        experience a new way to dine out.";
 include 'footer.html';
 ?>
