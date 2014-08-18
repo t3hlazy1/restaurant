2e91d95741b521701a2a34fdd8175e4e46864547
wjbz82 <wjbz82@mail.missouri.edu>
2014-08-18 02:25:41 -0500

    Added the registration form

--- register.php
+++ register.php
@@ -2,7 +2,11 @@
 	include 'includes.php';
 	include 'header.html';
 	
-	
+	echo "<form>
+			Select a username: <input type='text' name='username' maxlength='10'>
+			Select a password: <input type='password' name='password' maxlegnth='10'>
+			Re-enter your password: <input type='password' name='password' maxlength='10'>
+			</form>";
 	
 	
 	include 'footer.html';
