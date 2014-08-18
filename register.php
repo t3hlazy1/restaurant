0c609d1a4f2ee3ff5a72af41117744318b5be075
wjbz82 <wjbz82@mail.missouri.edu>
2014-08-18 02:28:23 -0500

    adjusted register.php / css on form

--- register.php
+++ register.php
@@ -2,10 +2,13 @@
 	include 'includes.php';
 	include 'header.html';
 	
-	echo "<form>
+	echo "<form id='register_form'>
 			Select a username: <input type='text' name='username' maxlength='10'>
+				</br>
 			Select a password: <input type='password' name='password' maxlegnth='10'>
+				</br>
 			Re-enter your password: <input type='password' name='password' maxlength='10'>
+				</br>
 			</form>";
 	
 	
