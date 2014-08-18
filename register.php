e4594e4496c31847658bd69775b9c6141ff12156
wjbz82 <wjbz82@mail.missouri.edu>
2014-08-18 02:31:37 -0500

    edited css some more

--- register.php
+++ register.php
@@ -2,14 +2,16 @@
 	include 'includes.php';
 	include 'header.html';
 	
-	echo "<form id='register_form'>
-			Select a username: <input type='text' name='username' maxlength='10'>
-				</br>
-			Select a password: <input type='password' name='password' maxlegnth='10'>
-				</br>
-			Re-enter your password: <input type='password' name='password' maxlength='10'>
-				</br>
-			</form>";
+	echo "<div id='reg_form'>
+			<form>
+				Select a username: <input type='text' name='username' maxlength='10'>
+					</br>
+				Select a password: <input type='password' name='password' maxlegnth='10'>
+					</br>
+				Re-enter your password: <input type='password' name='password' maxlength='10'>
+					</br>
+			</form>
+		</div>";
 	
 	
 	include 'footer.html';
