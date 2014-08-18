c7034fee50fd1fda25caf5288de6daeabc8d4634
wjbz82 <wjbz82@mail.missouri.edu>
2014-08-18 18:08:10 -0500

    updated process and register

--- process_register.php
+++ process_register.php
@@ -1,79 +1,91 @@
-<?php 
-
-	include 'includes.php'; 
-	
-	$error_msg = ""; 
-	
-	if(isset($_POST['name'], $_POST['password'], $_POST['checkpass'], $_POST['email'])){
-	
-		$ffusername = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING); 
-		$ffpassword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING); 
-		$ffcheckpass = filter_input(INPUT_POST, 'checkpass', FILTER_SANITIZE_STRING); 
-		$ffemail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING); 
-		
-		if($ffpassword != $ffcheckpass){
-			header("location: register.php"); 
-		}
-		
-		if(!filter_var($ffemail, FILTER_VALIDATE_EMAIL)){
-			$error_msg .= '<p>The email address you entered is not valid</p>'; 
-		}
-		
-		if(strlen($password) != 128){
-			$error_msg .= '<p>Invalid Password</p>'; 
-		}
-		
-		$prep_stmt = "SELECT id FROM users WHERE email = ? LIMIT 1";
-		$stmt = $con->prepare($prep_stmt); 
-		
-		if($stmt){
-			$stmt->bind_param('s', $ffemail); 
-			$stmt->execute(); 
-			$stmt->store_result(); 
-			
-			if($stmt->num_rows == 1){
-				$error_msg .= '<p>A user already exists with this email</p>';
-				$stmt->close(); 
-			}
-			$stmt->close(); 
-		}else{
-			$error_msg .= '<p>Database error, line 300</p>'; 
-			$stmt->close(); 
-		}
-		
-		$prep_stmt = "SELECT id FROM users WHERE name = ? LIMIT 1"; 
-		$stmt = $con->prepare($prep_stmt); 
-		
-		if($stmt){
-			$stmt->bind_param('s', $ffusername); 
-			$stmt->execute(); 
-			$stmt->store_result(); 
-				if($stmt->num_results == 1){
-					$error_msg .= '<p>A user with this name already exists</p>';
-					$stmt->close(); 
-				}
-			$stmt->close(); 
-		}else{
-			$error_msg .= '<p>Database error, line 300</p>'; 
-			$stmt->close();
-		}
-		
-		if(empty($error_msg)){
-			$random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true)); 
-			$password = hash('sha512', $ffpassword . $random_salt); 
-			
-			if($insert_stmt = $con->prepare("INSERT INTO users (name, email, password, salt VALUES (?, ?, ?, ?)")){
-				$insert_stmt->bind_param('ssss', $ffusername, $ffemail, $password, $random_salt); 
-					if(! $insert_stmt->execute()){
-					echo "test"; 
-				}else{
-					echo "it worked, yay, lel"; 
-				}
-			}
-			
-		}
-		
-	}else{
-		header("location: index.php"); 
-	}
-?>
+<?php
+include_once 'includes.php';
+ 
+$error_msg = "";
+ 
+if (isset($_POST['username'], $_POST['email'], $_POST['p'])) {
+    // Sanitize and validate the data passed in
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Not a valid email
+        $error_msg .= '<p class="error">The email address you entered is not valid</p>';
+    }
+ 
+    $password = filter_input(INPUT_POST, 'p', FILTER_SANITIZE_STRING);
+    if (strlen($password) != 128) {
+        // The hashed pwd should be 128 characters long.
+        // If it's not, something really odd has happened
+        $error_msg .= '<p class="error">Invalid password configuration.</p>';
+    }
+ 
+    // Username validity and password validity have been checked client side.
+    // This should should be adequate as nobody gains any advantage from
+    // breaking these rules.
+    //
+ 
+    $prep_stmt = "SELECT id FROM members WHERE email = ? LIMIT 1";
+    $stmt = $con->prepare($prep_stmt);
+ 
+   // check existing email  
+    if ($stmt) {
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $stmt->store_result();
+ 
+        if ($stmt->num_rows == 1) {
+            // A user with this email address already exists
+            $error_msg .= '<p class="error">A user with this email address already exists.</p>';
+                        $stmt->close();
+        }
+                $stmt->close();
+    } else {
+        $error_msg .= '<p class="error">Database error Line 39</p>';
+                $stmt->close();
+    }
+ 
+    // check existing username
+    $prep_stmt = "SELECT id FROM members WHERE username = ? LIMIT 1";
+    $stmt = $con->prepare($prep_stmt);
+ 
+    if ($stmt) {
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $stmt->store_result();
+ 
+                if ($stmt->num_rows == 1) {
+                        // A user with this username already exists
+                        $error_msg .= '<p class="error">A user with this username already exists</p>';
+                        $stmt->close();
+                }
+                $stmt->close();
+        } else {
+                $error_msg .= '<p class="error">Database error line 55</p>';
+                $stmt->close();
+        }
+ 
+    // TODO: 
+    // We'll also have to account for the situation where the user doesn't have
+    // rights to do registration, by checking what type of user is attempting to
+    // perform the operation.
+ 
+    if (empty($error_msg)) {
+        // Create a random salt
+        //$random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE)); // Did not work
+        $random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
+ 
+        // Create salted password 
+        $password = hash('sha512', $password . $random_salt);
+ 
+        // Insert the new user into the database 
+        if ($insert_stmt = $con->prepare("INSERT INTO users (name, email, password, salt) VALUES (?, ?, ?, ?)")) {
+            $insert_stmt->bind_param('ssss', $username, $email, $password, $random_salt);
+            // Execute the prepared query.
+            if (! $insert_stmt->execute()) {
+                header('Location: ../error.php?err=Registration failure: INSERT');
+            }
+        }
+        header('Location: ./register_success.php');
+    }
+}
