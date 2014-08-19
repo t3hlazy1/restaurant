776698ca156fed3bcf0f0f93b93ed3979315eb73
t3hlazy1 <lilfootballplaya12@hotmail.com>
2014-08-19 16:47:52 -0500

    Comment and update register page

--- register.php
+++ register.php
@@ -1,39 +1,54 @@
+//include jQuery
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+//Page scripts
 <script>
+
+//Disable submit button
 $("input[type=submit]").attr("disabled", "disabled");
-function nameCheck()
-{
+
+//Name Check
+//Determines if name is taken or not, and changes curr page accordingly
+function nameCheck(){
 var xmlhttp;    
-var str = $('input[name=name]').val();
-if (str=="")
-  {
-  //document.getElementById("name_info").innerHTML="";
+
+//Get username from form
+var name = $('input[name=name]').val();
+
+//If username is empty
+if (name=="" || name.length < 4){
+  //set input font color to red and disable submit button
+  $('input[name=name]').css("color", "red");
+  $("input[type=submit]").attr("disabled", "disabled");
   return;
-  }
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
+}
+
+if (window.XMLHttpRequest){
+// code for IE7+, Firefox, Chrome, Opera, Safari
   xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
+}
+else{
+// code for IE6, IE5
   xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    //document.getElementById("name_info").innerHTML=xmlhttp.responseText;
+}
+
+xmlhttp.onreadystatechange=function(){
+  if (xmlhttp.readyState==4 && xmlhttp.status==200){
+    //if username is available
     if (xmlhttp.responseText == "Available"){
       $('input[name=name]').css("color", "green");
       $("input[type=submit]").removeAttr("disabled");
     }
+    //if username is taken
     else{
       $('input[name=name]').css("color", "red");
       $("input[type=submit]").attr("disabled", "disabled");
     }
-    }
   }
-xmlhttp.open("GET","name_check.php?name="+str,true);
+}
+
+//open php script
+xmlhttp.open("GET","name_check.php?name="+name,true);
 xmlhttp.send();
 }
 </script>
@@ -47,7 +62,7 @@ xmlhttp.send();
       <form action='process_register.php' method='post'>
        <table>
         <tr>
-         <td>Name:</td> <td><input type='text' name='name' id='name' onkeyup='nameCheck()'></td><td id='name_info'></td><br>
+         <td>Name:</td> <td><input type='text' name='name' id='name' onkeyup='nameCheck()'></td><br>
         </tr>
         <tr>
          <td>Email:</td> <td><input type='text' name='email'></td><br>
